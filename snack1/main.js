d32373bdc5273983521c8fc6000e90f84d476701
Gaspare <Gas9220@users.noreply.github.com>
2023-03-08 18:39:24 +0100

    Creati gli oggetti vip con la chiave nome

--- snack1/main.js
+++ snack1/main.js
@@ -13,3 +13,13 @@ function createGuest(name) {
 
 //-----------------------------------------
 
+let bradPitt = createGuest("Brad Pitt");
+let johnnyDepp = createGuest("Johnny Depp");
+let ladyGaga = createGuest("Lady Gaga");
+let cristianoRonaldo = createGuest("Cristiano Ronaldo");
+let georginaRodriguez = createGuest("Georgina Rodriguez");
+let chiaraFerragni = createGuest("Chiara Ferragni");
+let fedez = createGuest("Fedez");
+let georgeClooney = createGuest("George Clooney");
+let amalClooney = createGuest("Amal Clooney");
+let maneskin = [createGuest("Damiano David"), createGuest("Victoria De Angelis"), createGuest("Thomas Raggi"), createGuest("Ethan Torchio")];
