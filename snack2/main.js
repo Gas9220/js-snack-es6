b9acded30b62b7ad96394109df42511dfb2488bc
Gaspare <Gas9220@users.noreply.github.com>
2023-03-08 19:08:49 +0100

    Funzione per creare l'oggetto studente

---
+++ snack2/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//------------- Funtions -------------------
+
+// Funzione che facilita la creazione di un singolo studente
+function createStudent(id, name, grade) {
+    let student = {
+        id: id,
+        name: name,
+        grade: grade
+    }
+
+    return student;
+}
+
+//-----------------------------------------
