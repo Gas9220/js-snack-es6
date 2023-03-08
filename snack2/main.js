d96bdd212bece4de78e13124b3d945948cbb770c
Gaspare <Gas9220@users.noreply.github.com>
2023-03-08 19:09:09 +0100

    Creati gli studenti

--- snack2/main.js
+++ snack2/main.js
@@ -14,3 +14,12 @@ function createStudent(id, name, grade) {
 }
 
 //-----------------------------------------
+
+// Creo gli studenti
+const marco = createStudent(213, 'Marco della Rovere', 78);
+const paola = createStudent(110, 'Paola Cortellessa', 96);
+const andrea = createStudent(250, 'Andrea Mantegna', 48);
+const gaia = createStudent(145, 'Gaia Borromini', 74);
+const luigi = createStudent(196, 'Luigi Grimaldello', 68);
+const piero = createStudent(102, 'Piero Della Francesca', 50);
+const francesca = createStudent(120, 'Francesca Da Polenta', 84);
