37623dbe7c5d56aa6ba87b5ef2f016c7ba7da9aa
Gaspare <Gas9220@users.noreply.github.com>
2023-03-08 18:38:49 +0100

    Funzione per creare invitato

---
+++ snack1/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//------------- Funtions -------------------
+
+// Funzione che facilita la creazione di un singolo invitato
+function createGuest(name) {
+    let guest = {
+        name: name,
+    }
+
+    return guest;
+}
+
+//-----------------------------------------
+
