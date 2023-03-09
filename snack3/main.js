7e1f9ef70ebf2dc7564cc1d47c7ad208d1513f34
Gaspare <Gas9220@users.noreply.github.com>
2023-03-09 15:08:54 +0100

    Create le biciclette

--- snack3/main.js
+++ snack3/main.js
@@ -14,4 +14,11 @@ function createBike(model, weight) {
 
 // -------------------------------------------
 
-
+// Creo le biciclette
+const scottSpeedster = createBike("Scott Speedster", 9);
+const canyonGrail = createBike("Canyon Grail", 8);
+const giantAdvanced = createBike("Giant Defy Advanced 2", 7.5);
+const bianchiSprint = createBike("Bianchi Sprint", 8.2);
+const treckEmonda = createBike("Treck Emonda", 9.7);
+const cannondale = createBike("Cannondale Carbon", 7.4);
+const bmcRoad = createBike("BMC Roadmachine", 11);
