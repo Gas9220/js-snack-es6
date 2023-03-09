dc1155e058f40e47a276a99fdbd2353f75921597
Gaspare <Gas9220@users.noreply.github.com>
2023-03-09 15:08:27 +0100

    Aggiunta funzione per creare le biciclette

---
+++ snack3/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Functions ---------------------------------
+
+// Funzione che facilita la creazione di una bicicletta
+function createBike(model, weight) {
+    const bike = {
+        model: model,
+        weight: weight
+    }
+
+    return bike;
+}
+
+// -------------------------------------------
+
+
