0375cd13ed9ffa8d5979d96ae9658f31edc3279c
Gaspare <Gas9220@users.noreply.github.com>
2023-03-09 15:57:40 +0100

    Creata funzione per creare una squadra

---
+++ snack4/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Functions ---------------------------------
+
+// Funzione che facilita la creazione di una squadra
+function createTeam(teamName) {
+    const team = {
+        teamName: teamName,
+        points: 0,
+        fouls: 0
+    }
+
+    return team;
+}
+
+// -------------------------------------------
